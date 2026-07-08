a9ba1de98af25033f0345a05741dd49cead1505d
Leon <office.ms.1387@gmail.com>
2026-07-08 22:27:22 +0330

    new func : commit_whole()

--- main.py
+++ main.py
@@ -20,3 +20,20 @@ def commit_single(file: str,gitname: str) -> bool:
         print(f"Error : {error}")
         return False
 
+def commit_whole(folder : str) -> bool:
+    try:
+        folder_name = os.path.basename(folder)
+        path = os.path.dirname(folder)
+        os.makedirs(f"mini_git/commit/{path}",exist_ok=True)
+        sh.copytree(folder,f"mini_git/commit/{path}/{folder_name}")
+        return True
+    except FileNotFoundError:
+        print("Source file not found")
+        return False
+    except PermissionError:
+        print("Permission denied")
+        return False
+    except Exception as error:
+        print(f"Error : {error}")
+        return False
+
