116b65ce7063679fdd03456a3664a9af19e5a190
Leon <office.ms.1387@gmail.com>
2026-07-16 20:48:18 +0330

    added git_commit_type

--- git_sqlite.py
+++ git_sqlite.py
@@ -7,14 +7,21 @@ def create_table_commit() -> None:
                        "id INTEGER PRIMARY KEY,"
                        "message TEXT DEFAULT 'none',"
                        "git_name TEXT NOT NULL,"
+                       "git_commit_type TEXT NOT NULL "
                        "timestamp TEXT DEFAULT CURRENT_TIMESTAMP"
                        ")")
 
-def insert_git_data(message: str,git_name: str,get_id: bool = True) -> int | None:
+def insert_git_data(message: str,git_name: str,commit_type: int,get_id: bool = True) -> int | None:
     with sql.connect("git.db") as con:
         cursor = con.cursor()
         try:
-            cursor.execute("INSERT INTO commits (message,git_name) VALUES (?,?)", [message, git_name])
+            if commit_type == 1:
+                commit_type = "Single Commit"
+            elif commit_type == 2:
+                commit_type = "Full Commit"
+            else:
+                raise "Wrong commit_type number inserted"
+            cursor.execute("INSERT INTO commits (message,git_name,git_commit_type) VALUES (?,?,?)", [message, git_name,commit_type])
             if not get_id:
                 return None
             #cursor.execute("SELECT id FROM commits ORDER BY id DESC")
